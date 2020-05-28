ad22326f7917a730ff55c5ad5b71bb6dfe055cd1
Martin Sakac <martinsakac@Martins-MacBook-Pro.local>
2020-05-28 09:59:57 +0200

    Setup production build and server

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -6,7 +6,11 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 );
 
